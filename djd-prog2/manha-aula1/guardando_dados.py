e5547ff6e3960a58b5fc9a99364cb4d2627513d2
Antonio Carvalho <antoniorcn@hotmail.com>
2020-08-24 13:01:48 -0300

    Aula 1 de PROG2 manha

---
+++ djd-prog2/manha-aula1/guardando_dados.py
@@ -0,0 +1,6 @@
+txt = "Este texto será guardado na memória"
+n = 10
+f = 50.4
+log = True
+log2 = False
+print(txt)
---
+++ djd-prog2/manha-aula1/variaveis_string.py
@@ -0,0 +1,6 @@
+texto = "Alunos de Jogos Digitais Fatec - 2o Semestre"
+print("Maiusculo: ", texto.upper())
+print("Minusculo: ", texto.lower())
+print("Quantidade de caracteres: ", len(texto))
+print("Há a palavra Fatec no texto: ", texto.find("Fatec"))
+print("Há a palavra Carapicuíba no texto: ", texto.find("Carapicuíba"))
