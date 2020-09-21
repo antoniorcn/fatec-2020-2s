3f1a5450da1494bdbf25ed6fb0af840ba20fd4a5
Antonio Carvalho <antoniorcn@hotmail.com>
2020-09-21 12:41:51 -0300

    Aula 4 PROG2 Manha'

---
+++ djd-prog2/manha-aula4/temperatura.py
@@ -0,0 +1,33 @@
+print("Inicio do programa")
+temperatura = -30
+# if temperatura > 30:
+#     print("Esta muito calor")
+# else:
+#     if temperatura > 20:
+#         print("Esta calor")
+#     else:
+#         if temperatura > 15:
+#             print("Agradavel")
+#         else:
+#             if temperatura > 5:
+#                 print("Esta frio")
+#             else:
+#                 if temperatura > -10:
+#                     print("Está muito frio")
+#                 else:
+#                     print("Congelando")
+
+if temperatura > 30:
+    print("Esta muito calor")
+elif temperatura > 20:
+    print("Esta calor")
+elif temperatura > 15:
+    print("Agradavel")
+elif temperatura > 5:
+    print("Esta frio")
+elif temperatura > -10:
+    print("Está muito frio")
+else:
+    print("Congelando")
+
+print("Fim do programa")
