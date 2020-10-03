8765055ca0dfb93530b6abe6ac3ccfffd03cc97a
Antonio Carvalho <antoniorcn@hotmail.com>
2020-10-03 10:01:58 -0300

    Aula 6 PROG2 Noite

---
+++ djd-prog2/noite-aula6/teste-for.py
@@ -0,0 +1,7 @@
+print("inicio do programa")
+
+for _ in range(1, 11, 1):
+    print("Mario - Kart")
+    print(".")
+
+print("fim do programa")
---
+++ djd-prog2/noite-aula6/teste-while.py
@@ -0,0 +1,13 @@
+print("inicio do programa")
+
+a = 1           # Inicialização
+while a <= 10:  # Condição
+    print("Mario - Kart")
+    print(".")
+    a = a + 1   # Incremento
+
+print("fim do programa")
+
+
+
+
