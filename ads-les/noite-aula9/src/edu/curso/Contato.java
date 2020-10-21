406892010fea139bb4b9a133e8e9061089be026c
Antonio Carvalho <antoniorcn@hotmail.com>
2020-10-21 20:59:51 -0300

    LES Noite aula 9 parte 2

---
+++ ads-les/noite-aula9/src/edu/curso/Contato.java
@@ -0,0 +1,36 @@
+package edu.curso;
+
+public class Contato {
+	private Long id;
+	private String nome;
+	private String telefone;
+	private String email;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	 
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- ads-les/noite-aula9/src/edu/curso/ContatoController.java
+++ ads-les/noite-aula9/src/edu/curso/ContatoController.java
@@ -2,12 +2,33 @@ package edu.curso;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContatoController {
 
 	@GetMapping("/contato")
-	public String getContatos() { 
-		return "contato";
+	public ModelAndView getContato() {
+		ModelAndView mv = new ModelAndView("contato");
+		
+		Contato c = new Contato();
+		c.setNome("Antonio");
+		c.setTelefone("(11) XXXX-YYYY");
+		
+		mv.addObject("CONTATO_ATUAL", c);
+		
+		return mv;
+	}
+	
+	
+	@PostMapping("/contato")
+	public ModelAndView postContato(
+			@ModelAttribute("CONTATO_ATUAL") Contato c) {
+		System.out.println(c.getNome());
+		System.out.println(c.getTelefone());
+		System.out.println(c.getEmail());
+		return new ModelAndView("contato", "CONTATO_ATUAL", new Contato());
 	}
 }
