097dcb41b624b15ebb2eafe3484049f5ccfb54ce
Antonio Carvalho <antoniorcn@hotmail.com>
2020-11-16 13:03:45 -0300

    AULA 10 PROG2 manha parte 2

--- djd-prog2/manha-aula10/animacao_sprite.py
+++ djd-prog2/manha-aula10/animacao_sprite.py
@@ -1,8 +1,8 @@
 import pygame
 
 tela = pygame.display.set_mode((800, 600))
-
 imgSpriteSheet = pygame.image.load("./EpicArmor.png")
+estado = "JOGANDO"
 
 
 def pegar_imagem_por_gid(spritesheet, gid=0, largura=64, altura=64,
@@ -22,9 +22,14 @@ class Cavaleiro(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.frame_indice = 0
         self.andando_cima = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.parado_cima = [0]
         self.andando_baixo = [19, 20, 21, 22, 23, 24, 25, 26]
+        self.parado_baixo = [18]
         self.andando_esquerda = [10, 11, 12, 13, 14, 15, 16, 17]
+        self.parado_esquerda = [9]
         self.andando_direita = [28, 29, 30, 31, 32, 33, 34, 35]
+        self.parado_direita = [27]
+
         self.lista_frames = self.andando_esquerda
         self.image = self.get_frame()
         self.rect = pygame.Rect((200, 200), self.image.get_size())
@@ -40,19 +45,43 @@ class Cavaleiro(pygame.sprite.Sprite):
     def update(self):
         self.image = self.get_frame()
 
+    def processar_evento(self, e):
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_LEFT:
+                self.lista_frames = self.andando_esquerda
+            if e.key == pygame.K_RIGHT:
+                self.lista_frames = self.andando_direita
+            if e.key == pygame.K_UP:
+                self.lista_frames = self.andando_cima
+            if e.key == pygame.K_DOWN:
+                self.lista_frames = self.andando_baixo
+        if e.type == pygame.KEYUP:
+            if e.key == pygame.K_LEFT:
+                self.lista_frames = self.parado_esquerda
+            if e.key == pygame.K_RIGHT:
+                self.lista_frames = self.parado_direita
+            if e.key == pygame.K_UP:
+                self.lista_frames = self.parado_cima
+            if e.key == pygame.K_DOWN:
+                self.lista_frames = self.parado_baixo
 
-player = Cavaleiro()
-jogadores = pygame.sprite.Group(player)
+
+player1 = Cavaleiro()
+player2 = Cavaleiro()
+player2.rect.topleft = (300, 300)
+jogadores = pygame.sprite.Group(player1, player2)
 jogar = True
 clk = pygame.time.Clock()
 while jogar:
     # Calcular regras
-    jogadores.update()
+    if estado == "JOGANDO":
+        jogadores.update()
 
     # Pintar na tela
-    tela.fill((0, 0, 0))
-    jogadores.draw(tela)
-    pygame.display.update()
+    if estado == "JOGANDO":
+        tela.fill((0, 0, 0))
+        jogadores.draw(tela)
+        pygame.display.update()
 
     clk.tick(10)
 
@@ -61,9 +90,13 @@ while jogar:
         if e.type == pygame.QUIT:
             jogar = False
         if e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_LEFT:
-                player.lista_frames = player.andando_esquerda
-            if e.key == pygame.K_RIGHT:
-                player.lista_frames = player.andando_direita
+            if e.key == pygame.K_ESCAPE:
+                if estado == "JOGANDO":
+                    estado = "PAUSADO"
+                else:
+                    estado = "JOGANDO"
+        player1.processar_evento(e)
+        player2.processar_evento(e)
+
 
 
