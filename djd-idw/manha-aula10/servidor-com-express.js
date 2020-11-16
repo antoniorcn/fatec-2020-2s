d599476eb5db2bbf71315e53d9ad0d53e29cf480
Antonio Carvalho <antoniorcn@hotmail.com>
2020-11-16 09:06:01 -0300

    Aula 10 IDW Parte 2

--- djd-idw/manha-aula10/servidor-com-express.js
+++ djd-idw/manha-aula10/servidor-com-express.js
@@ -1,32 +1,18 @@
 const express = require('express');
 const app = express();
-// function teste(request, response) { 
-//     response.send("<h1>Teste de Pagina</h1>");
-// }
-
 app.use(express.static('./public'));
-
 app.set('view engine', 'hbs');
-
 const teste = (request, response) => {
     console.log("Função anonima");
     response.send("<h1>Teste de página com função anônima</h1>")
 }
-
-
-
 app.get("/teste", teste);
-
 app.get("/bomdia", (request, response) => { 
+    let nomeCompleto = request.query.nomecompleto;
     const agora = Date();
-    response.render("teste", {nome: agora});
+    response.render("teste", {nome: nomeCompleto, horas:agora});
     
 })
-
-// function callback() { 
-//     console.log("Servidor iniciado");
-// }
-
 app.listen(80, () => {
     console.log("Servidor iniciado");
 });
